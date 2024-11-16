6e369bebff3434437f1f7671e5dce8e001555e57
Kyle Chi <kylechi2157@gmail.com>
2024-11-16 13:58:38 -0600

    citizen init'

--- Scripts/citizen.gd
+++ Scripts/citizen.gd
@@ -1,1 +1,43 @@
-extends Node
+class_name Citizen
+
+var name: String
+var gender: String
+var immunity: float
+var isVaccinated: bool
+var isMasked: bool
+var isEducated: bool
+var job: Vector2
+var residence: Vector2
+
+func _init():
+	pass
+
+func getVaccinated():
+	return isVaccinated
+	
+func setVaccinated(vacc: bool):
+	isVaccinated = vacc
+	
+func getMasked():
+	return isMasked
+	
+func setMasked(mask: bool):
+	isMasked = mask
+	
+func getEducated():
+	return isEducated
+
+func setEducated(edu: bool):
+	isEducated = edu
+	
+func getJub():
+	return job
+
+func setJob(loc: Vector2):
+	job = loc
+	
+func getResidence():
+	return residence
+
+func setResidence(res: Vector2):
+	residence = res
--- Scripts/player_creation.gd
+++ Scripts/player_creation.gd
@@ -3,6 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var people = Controller.num_people
 	pass # Replace with function body.
 	
 
