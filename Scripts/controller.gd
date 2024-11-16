5de5385ee8b8b193bb0b94b06a352bf87242a8fb
fdeevy <findeevy@gmail.com>
2024-11-16 16:35:26 -0600

    Added AStar

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -2,9 +2,12 @@ extends Node
 
 var MAP_SIZE = Vector2(44,30)
 
+
+var places = {}
 var tile_array_0 = []
 var num_people = 40
 var collision_data = []
+var citizen_astar = AStar2D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
