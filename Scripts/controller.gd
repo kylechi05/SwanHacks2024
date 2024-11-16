ab7112e32db38ed3a68cc8fb8d8f429da35e2a6a
Kyle Chi <kylechi2157@gmail.com>
2024-11-16 16:34:52 -0600

    people created except job and home

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -2,6 +2,8 @@ extends Node
 
 var num_people = 40
 var edu_prob = 0.3
+var immunity_window = [0.05, 0.70]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- Scripts/player_creation.gd
+++ Scripts/player_creation.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	
 	var num_people = Controller.num_people
 	var edu_prob = Controller.edu_prob
-	var imm_probs = generateSkewedProbabilities(num_people, 0.05, 0.70, 0.30)
+	var imm_probs = generateProbabilities(num_people, Controller.immunity_window[0], Controller.immunity_window[1])
 	var names = load_json_file("res://Data/citizen_names.json").names
 	
 	names.shuffle()
@@ -15,11 +15,12 @@ func _ready() -> void:
 		var cit = Citizen.new(names[i], imm_probs[i], edu_prob)
 		citizens.append(cit)	
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func generateSkewedProbabilities(count: int, minimum: float, maximum: float, average: float) -> Array:
+func generateProbabilities(count: int, minimum: float, maximum: float) -> Array:
 	var probabilities = []
 	
 	for i in range(count):
